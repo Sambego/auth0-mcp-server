01a7147e261de6c5da3cb28671d9722fd9deb8c1
Sambego <sambellen@gmail.com>
2026-01-14 10:01:36 +0100

    Fix tests

--- src/auth/device-auth-flow.ts
+++ src/auth/device-auth-flow.ts
@@ -27,7 +27,7 @@ function getConfig(selectedScopes?: string[]) {
   };
 }
 
-async function requestAuthorization(selectedScopes?: string[], interaction?: boolean) {
+async function requestAuthorization(selectedScopes?: string[], interaction: boolean = true) {
   const config = getConfig(selectedScopes);
   const body: any = {
     client_id: config.clientId,
